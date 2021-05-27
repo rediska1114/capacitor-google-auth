fcdecd75ccff26076bfd3d4663f14cfa651dd678
Stepan Kotov <rediska1114@gmail.com>
2021-05-27 19:47:06 +0300

    fix ngx

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,11 +1,10 @@
 import { GoogleAuthPlugin } from './definitions';
-import { Injectable } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 
 const GoogleAuthPlugin =
   Plugins.GoogleAuth as GoogleAuthPlugin;
 
-@Injectable()
+
 export class GoogleAuth implements GoogleAuthPlugin {
   signIn = GoogleAuthPlugin.signIn;
   refresh = GoogleAuthPlugin.refresh;
